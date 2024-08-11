e2dacb72d8ff7d83587029ef339458505cd05662
Javier Bac <javierbac010@gmail.com>
2024-08-11 11:51:52 -0600

    Permiso a todo

--- src/main/java/Ada_School/Proyecto_Integrador/config/SecurityConfig.java
+++ src/main/java/Ada_School/Proyecto_Integrador/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authRequest ->
                         authRequest
-                                .requestMatchers("/auth/**").permitAll()
+                                .requestMatchers("/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManager ->
